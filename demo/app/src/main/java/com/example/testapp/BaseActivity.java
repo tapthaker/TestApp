80e33bd994f9e203a4947d9742e8196d882d6f88
Rahul Sharma <rahul.taicho@gmail.com>
2017-07-04 23:51:49 +0530

    Moves onCreate to BaseActivity

--- demo/app/src/main/java/com/example/testapp/BaseActivity.java
+++ demo/app/src/main/java/com/example/testapp/BaseActivity.java
@@ -1,7 +1,18 @@
 package com.example.testapp;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+  private static final String TAG = BaseActivity.class.getSimpleName();
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Log.d(TAG, "Created activity");
+  }
 }
--- demo/app/src/main/java/com/example/testapp/MainActivity.java
+++ demo/app/src/main/java/com/example/testapp/MainActivity.java
@@ -1,18 +1,6 @@
 package com.example.testapp;
 
-import android.os.Bundle;
-import android.util.Log;
-
 public class MainActivity extends BaseActivity {
-
-  private static final String TAG = MainActivity.class.getSimpleName();
-  
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    Log.d(TAG, "Created activity");
-  }
 
   @Override
   protected void onResume() {
