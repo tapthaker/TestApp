b79b4fc1f910bbca2b6854dde309f71c65a56224
Rahul Sharma <rahul.taicho@gmail.com>
2017-07-04 23:50:35 +0530

    Extends MainActivity from BaseActivity

--- demo/app/src/main/java/com/example/testapp/MainActivity.java
+++ demo/app/src/main/java/com/example/testapp/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.testapp;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
   private static final String TAG = MainActivity.class.getSimpleName();
   
