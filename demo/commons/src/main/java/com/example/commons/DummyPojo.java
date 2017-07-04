9530651cf897a6fb090046905ceae371fe083256
Rahul Sharma <rahul.taicho@gmail.com>
2017-07-04 23:10:49 +0530

    Adds dummy Pojo to commons

---
+++ demo/commons/src/main/java/com/example/commons/DummyPojo.java
@@ -0,0 +1,22 @@
+package com.example.commons;
+
+
+public class DummyPojo {
+
+  private String name;
+
+  public DummyPojo(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public String toString() {
+    return "DummyPojo{" +
+        "name='" + name + '\'' +
+        '}';
+  }
+}
