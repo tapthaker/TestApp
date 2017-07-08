e4468d2617daef3656394db6da705bd0c784cb64
Rahul Sharma <rahul.taicho@gmail.com>
2017-07-08 19:12:15 +0530

    Commons #2 Breaks UI

--- commons/src/main/java/com/example/commons/TestPojo.java
+++ commons/src/main/java/com/example/commons/TestPojo.java
@@ -1,11 +1,9 @@
 package com.example.commons;
 
-import java.util.ArrayList;
-
 public class TestPojo {
   private String name;
 
-  public TestPojo(ArrayList<String> namePojo) {
+  public TestPojo(String namePojo) {
 
   }
 }
