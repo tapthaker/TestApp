dde6b56803388209e13c3029fe001ab6dc1251ba
Rahul Sharma <rahul.taicho@gmail.com>
2017-07-08 20:27:03 +0530

    Uikit uses commons

--- network/src/main/java/com/example/network/NetworkPojo.java
+++ network/src/main/java/com/example/network/NetworkPojo.java
@@ -1,13 +1,10 @@
 package com.example.network;
 
 
-import com.example.commons.TestPojo;
-
 public class NetworkPojo {
   private String namePojo;
 
   public NetworkPojo(String namePojo) {
     this.namePojo = namePojo;
-    TestPojo pojo = new TestPojo("");
   }
 }
