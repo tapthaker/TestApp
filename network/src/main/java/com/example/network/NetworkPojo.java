c382df82f930fd627ab5da8895e04a28f2d5cbb4
Rahul Sharma <rahul.taicho@gmail.com>
2017-07-08 20:22:31 +0530

    Fixes NW#1

--- network/src/main/java/com/example/network/NetworkPojo.java
+++ network/src/main/java/com/example/network/NetworkPojo.java
@@ -3,13 +3,11 @@ package com.example.network;
 
 import com.example.commons.TestPojo;
 
-import java.util.ArrayList;
-
 public class NetworkPojo {
   private String namePojo;
 
   public NetworkPojo(String namePojo) {
     this.namePojo = namePojo;
-    TestPojo pojo = new TestPojo(new ArrayList<String>());
+    TestPojo pojo = new TestPojo("");
   }
 }
