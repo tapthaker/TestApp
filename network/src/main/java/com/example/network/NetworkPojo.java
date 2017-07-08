84d9b136ffd5a17ad711d7b23c0e9ef0ecc94d9a
Rahul Sharma <rahul.taicho@gmail.com>
2017-07-08 20:12:59 +0530

    Uses Commons in NW

--- network/src/main/java/com/example/network/NetworkPojo.java
+++ network/src/main/java/com/example/network/NetworkPojo.java
@@ -1,10 +1,15 @@
 package com.example.network;
 
 
+import com.example.commons.TestPojo;
+
+import java.util.ArrayList;
+
 public class NetworkPojo {
   private String namePojo;
 
   public NetworkPojo(String namePojo) {
     this.namePojo = namePojo;
+    TestPojo pojo = new TestPojo(new ArrayList<String>());
   }
 }
