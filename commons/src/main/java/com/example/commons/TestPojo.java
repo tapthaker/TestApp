4fc825a7d64971c38670610ef631aa78f19553f4
Rahul Sharma <rahul.taicho@gmail.com>
2017-07-08 14:08:45 +0530

    Adds module dependencies to build files

--- commons/src/main/java/com/example/commons/TestPojo.java
+++ commons/src/main/java/com/example/commons/TestPojo.java
@@ -2,4 +2,8 @@ package com.example.commons;
 
 public class TestPojo {
   private String name;
+
+  public TestPojo(String namePojo) {
+
+  }
 }
--- network/src/main/java/com/example/network/NetworkPojo.java
+++ network/src/main/java/com/example/network/NetworkPojo.java
@@ -1,6 +1,13 @@
 package com.example.network;
 
 
+import com.example.commons.TestPojo;
+
 public class NetworkPojo {
   private String namePojo;
+
+  public NetworkPojo(String namePojo) {
+    this.namePojo = namePojo;
+    new TestPojo(namePojo);
+  }
 }
